26fed518d0e1f0cad9b5a7b217d909ed48d8a952
kaushal-pandya <74071394+kaushal-pandya@users.noreply.github.com>
2022-02-22 13:32:07 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports =
 {
     //"URI" : "mongodb://localhost/contact_list"
-    "URI": "mongodb+srv://Kaushalpandya:KtrKtB6VVigu5AOs@cluster001.hkjrg.mongodb.net/test?authSource=admin&replicaSet=atlas-ibfkvt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+    "URI": "mongodb+srv://Kaushalpandya:KtrKtB6VVigu5AOs@cluster001.hkjrg.mongodb.net/BusinessContact?authSource=admin&replicaSet=atlas-ibfkvt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
     "Secret" : 'Kaushalkey'
 }
